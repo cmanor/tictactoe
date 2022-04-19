f0073f394336b8bba6083996a7e18e55faf28bf9
cmanor <101116379+cmanor@users.noreply.github.com>
2022-04-19 18:25:41 -0400

    working now

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import calculateWinner from '../helpers';
 import Board from './Board';
 
+const style = {
+    width: '200px',
+    margin: '20px auto',
+}
 
 const Game =() => {
     const [board, setBoard] = useState(Array(9).fill(null));
@@ -14,7 +18,7 @@ const Game =() => {
         // If user clicks occupied square or if game is won, do nothing
         if (winner || boardCopy[i]) return;
         //put an X or O in the clicked square
-        boardCopy[i] = xIsNext ? 'X' : 'O';
+        boardCopy[i] = xIsNext ? 'Deez' : 'Nuts';
         setBoard(boardCopy);
         setXIsNext(!xIsNext);
     };
@@ -23,12 +27,21 @@ const Game =() => {
 
     };
 
-    const renderMoves = {
-
-    };
+    const renderMoves = () => 
+         <button onClick={() => setBoard(Array(9).fill(null))}>
+        New Game
+        </button>
+    ;
 
     return (
+        //you can wrap elements in fragments if you don't want to render the element
+        <>
         <Board squares = {board} onClick={handleClick} />
+        <div style={style}>
+            <p>{winner ? 'Winner: ' + winner : 'Next Player :' + (xIsNext ? 'X' : 'O')}</p>
+            {renderMoves()}
+        </div>
+        </>
     )
 
 };
